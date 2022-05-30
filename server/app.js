7f9f6a821f6cdd5da6c4ac2aaea3a47d5dae4de8
GigiDev7 <gigivadachkoria@gmail.com>
2022-05-30 17:20:42 +0400

    FEAT: added login and register routes

--- server/app.js
+++ server/app.js
@@ -2,6 +2,13 @@ const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
 
+const userRouter = require("./routes/user");
+
 const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+app.use("/api/user", userRouter);
 
 module.exports = app;
