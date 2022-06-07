b6c23306e5e7659ed85749f588285301b69c3f96
GigiDev7 <gigivadachkoria@gmail.com>
2022-06-07 12:35:35 +0400

    FEAT: added validations for flights services

--- server/controllers/flights.js
+++ server/controllers/flights.js
@@ -5,8 +5,14 @@ const {
   findFlights,
 } = require("../services/flights");
 
+const { validationResult } = require("express-validator");
+
 const addFlight = async (req, res, next) => {
   try {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
     const newFlight = await createFlight(req.body);
     res.status(200).json(newFlight);
   } catch (error) {
