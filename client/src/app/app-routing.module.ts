9c0a5d38b77d1ce65365f3227529b4f279c9266b
GigiDev7 <gigivadachkoria@gmail.com>
2022-06-13 17:33:46 +0400

    FEAT: added guard to restrict acces to login and register page if user is already logged in

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AuthGuard } from './auth/auth.guard';
+import { AuthPageRestrict } from './auth/authPageRestrict.guard';
 import { LoginComponent } from './auth/login/login.component';
 import { RegisterComponent } from './auth/register/register.component';
 import { FlightsComponent } from './flights/flights.component';
@@ -9,8 +10,12 @@ import { NotFoundComponent } from './not-found/not-found.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent, canActivate: [AuthGuard] },
-  { path: 'login', component: LoginComponent },
-  { path: 'register', component: RegisterComponent },
+  { path: 'login', component: LoginComponent, canActivate: [AuthPageRestrict] },
+  {
+    path: 'register',
+    component: RegisterComponent,
+    canActivate: [AuthPageRestrict],
+  },
   { path: 'flights', component: FlightsComponent, canActivate: [AuthGuard] },
   { path: '**', component: NotFoundComponent },
 ];
