fae87ec21cbc72a9b225707e501a422a30e0d3ec
GigiDev7 <gigivadachkoria@gmail.com>
2022-06-14 16:45:39 +0400

    FEAT: styling admin page

--- client/src/app/admin/admin.module.ts
+++ client/src/app/admin/admin.module.ts
@@ -1,3 +1,4 @@
+import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
 import { AdminComponent } from './admin.component';
@@ -5,7 +6,7 @@ import { SideNavComponent } from './side-nav/side-nav.component';
 
 @NgModule({
   declarations: [AdminComponent, SideNavComponent],
-  imports: [MatIconModule],
+  imports: [MatIconModule, CommonModule],
   exports: [AdminComponent, SideNavComponent],
 })
 export class AdminModule {}
--- client/src/app/admin/side-nav/side-nav.component.ts
+++ client/src/app/admin/side-nav/side-nav.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import { UserModel } from 'src/app/shared/models/userModel';
 
 @Component({
   selector: 'app-side-nav',
   templateUrl: './side-nav.component.html',
-  styleUrls: ['./side-nav.component.sass']
+  styleUrls: ['./side-nav.component.css'],
 })
 export class SideNavComponent implements OnInit {
+  public user!: UserModel;
+  public isSideBarClosed: boolean = false;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  public handleSideBarToggle(): void {
+    this.isSideBarClosed = !this.isSideBarClosed;
   }
 
+  constructor() {}
+
+  ngOnInit(): void {
+    this.user = JSON.parse(localStorage.getItem('user')!);
+  }
 }
