3605d721b2e1c57f9ffccd39af91cadb5e4efb5a
GigiDev7 <gigivadachkoria@gmail.com>
2022-06-24 14:28:51 +0400

    FEAT: added dummy booking page and route

---
+++ client/src/app/bookings/bookings.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-bookings',
+  templateUrl: './bookings.component.html',
+  styleUrls: ['./bookings.component.sass']
+})
+export class BookingsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
