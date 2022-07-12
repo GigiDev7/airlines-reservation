8706c9a2abab5e3fc6ce06f5c5fba50028301047
GigiDev7 <gigivadachkoria@gmail.com>
2022-07-12 12:19:15 +0400

    FEAT: added unit testing for header component

---
+++ client/src/app/header/header.component.spec.ts
@@ -0,0 +1,60 @@
+import { HeaderComponent } from './header.component';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { AuthService } from '../auth/auth.service';
+import { Router } from '@angular/router';
+
+describe('HeaderComponent', () => {
+  let component: HeaderComponent;
+  let fixture: ComponentFixture<HeaderComponent>;
+  let authService: AuthService;
+  let router: Router;
+
+  beforeEach(() => {
+    const authServiceStub = {
+      logout: () => {},
+    };
+
+    const routerSpy = jasmine.createSpyObj('Router', ['navigate']);
+
+    TestBed.configureTestingModule({
+      declarations: [HeaderComponent],
+      providers: [
+        { provide: AuthService, useValue: authServiceStub },
+        { provide: Router, useValue: routerSpy },
+      ],
+    });
+
+    fixture = TestBed.createComponent(HeaderComponent);
+    component = fixture.componentInstance;
+    authService = TestBed.inject(AuthService);
+    router = TestBed.inject(Router);
+  });
+
+  it('component should be created', () => {
+    expect(component).toBeTruthy();
+    expect(component.user).toEqual(null);
+    expect(component.isWindowShown).toBe(false);
+  });
+
+  it('should toggle window state', () => {
+    component.toggleWindow();
+    expect(component.isWindowShown).toBe(true);
+    component.toggleWindow();
+    expect(component.isWindowShown).toBe(false);
+  });
+
+  it('should log out', () => {
+    spyOn(authService, 'logout');
+    component.handleLogout();
+    expect(authService.logout).toHaveBeenCalled();
+    expect(router.navigate).toHaveBeenCalledWith(['login']);
+  });
+
+  it('should get user from localstorage', () => {
+    spyOn(localStorage, 'getItem').and.returnValue('userData');
+    spyOn(JSON, 'parse');
+    component.ngOnInit();
+    expect(localStorage.getItem).toHaveBeenCalledWith('user');
+    expect(component.user).not.toBe(null);
+  });
+});
